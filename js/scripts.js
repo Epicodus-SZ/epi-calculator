4b96682fe0db08e355360c16b81f127703a2e15d
Emilie Thoreson and Steve Zaske <emilie@>
2017-06-01 16:24:07 -0700

    add operator branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var subtraction = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
@@ -17,32 +17,25 @@ var divide = function(number1, number2) {
 
 // Everything below this line is user interface (or front-end) logic:
 $(document).ready(function() {
-  $("#addButton").click(function(event) {
+
+  $("#goButton").click(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    $("div#output").text(add(number1, number2));
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var results;
+
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("div#output").text(result);
   });
-  $("#multiplyButton").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    $("div#output").text(multiply(number1, number2));
-  });
-    $("#divisionButton").click(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#add1").val());
-      var number2 = parseInt($("#add2").val());
-      $("div#output").text(divide(number1, number2));
-  });
-  $("#subtractionButton").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    $("div#output").text(subtraction(number1, number2));
 
 
-
-
-  });
 });
